30f4f500608f7f73d01c2c4f9171a770babc1266
Dhawal M <dhawalmehta91@gmail.com>
2018-04-05 20:46:05 +0530

    new session added

--- presentation/index.js
+++ presentation/index.js
@@ -23,7 +23,7 @@ require("normalize.css");
 const theme = createTheme({
   primary: "white",
   secondary: "#1F2022",
-  tertiary: "#03A9FC",
+  tertiary: "#ff60ae",
   quarternary: "#CECECE"
 }, {
   primary: "Montserrat",
@@ -39,107 +39,94 @@ export default class Presentation extends React.Component {
             Javascript Course
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            Session 1 - 3rd April, 2018
+            Session 2 - 5rd April, 2018
           </Text>
         </Slide>
 
         <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>Session Target</Heading>
-          <Heading size={5} textColor="secondary">Introduction to Javascript</Heading>
-          <Heading size={5} textColor="secondary">Display Possibilities</Heading>
-          <Heading size={5} textColor="secondary">Variables</Heading>
+          <Heading size={5} textColor="secondary">Recap</Heading>
+          <Heading size={5} textColor="secondary">Operators</Heading>
+          <Heading size={5} textColor="secondary">Expressions/Statements</Heading>
           <Heading size={5} textColor="secondary">Q&A</Heading>
           <Heading size={5} textColor="secondary">Assignment Questions</Heading>
         </Slide>
 
-        <Slide transition={["zoom"]} bgColor="tertiary">
-          <Heading size={3} textColor="primary" caps fit>Introduction to javascript</Heading>
-        </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={3} textColor="secondary" caps fit>JavaScript Can Change/Hide/Show HTML Content:</Heading>
+          <Heading size={3} textColor="secondary" caps fit>What will be the solution of:</Heading>
           <List>
-            <ListItem>document.getElementById("demo").innerHTML = "Hello JavaScript";</ListItem>
-            <ListItem>document.getElementById("demo").style.display = "none";</ListItem>
-            <ListItem>document.getElementById("demo").style.display = "block";</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={3} textColor="secondary" caps fit>JavaScript Can Change HTML Attributes:</Heading>
-          <List>
-            <ListItem>Changing the src (source) attribute of an {"<img>"} tag</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={3} textColor="secondary" caps fit>JavaScript Can Change HTML Styles (CSS):</Heading>
-          <List>
-            <ListItem>document.querySelector('body').style.fontSize = "50px";</ListItem>
-            <ListItem>Can change any css properties</ListItem>
+            <ListItem>var x = 25 + "apples";</ListItem>
+            <ListItem>var x = "25" + "apples";</ListItem>
+            <ListItem>var x = 25 + 25 + "apples";</ListItem>
+            <ListItem>var x = 25 + "apples" + 25 ;</ListItem>
+            <ListItem>var x = "apples" + 25 + 25;</ListItem>
           </List>
         </Slide>
 
+
         <Slide transition={["zoom"]} bgColor="tertiary">
-          <Heading size={3} textColor="primary" caps fit>Ways to Include JavaScript in your webpage</Heading>
+          <Heading size={3} textColor="primary" caps fit>Operators</Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
           <List>
-            <ListItem>Head or body?</ListItem>
-            <ListItem>Internal/Embedded (using {'<script>'})</ListItem>
-            <ListItem>External (.js extenstion)</ListItem>
+            <ListItem>Arithmetic operators</ListItem>
+            <ListItem>Comparison operators</ListItem>
+            <ListItem>Logical operators</ListItem>
+            <ListItem>Conditional (ternary) operator</ListItem>
+            <ListItem>Assignment operators</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={3} textColor="secondary" caps fit>Arithmetic operators:</Heading>
+          <List>
+            <ListItem>addition (+), subtraction (-), multiplication (*), division (/)</ListItem>
+            <ListItem>Modulus (Remainder) % </ListItem>
+            <ListItem>Increment (++)</ListItem>
+            <ListItem>Decrement (--)</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={3} textColor="secondary" caps fit>Comparison Operators:</Heading>
+          <List>
+            <ListItem>== equal to</ListItem>
+            <ListItem>=== equal value and equal type</ListItem>
+            <ListItem>!= not equal</ListItem>
+            <ListItem>Greater than {'>'}, Less than {'<'}, greater than or equal to {'>='}, less than or equal to {'<='}</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={3} textColor="secondary" caps fit>Logical Operator</Heading>
+          <List>
+            <ListItem>&& and</ListItem>
+            <ListItem>|| or</ListItem>
+            <ListItem>!</ListItem>
           </List>
         </Slide>
 
+
         <Slide transition={["zoom"]} bgColor="tertiary">
-          <Heading size={3} textColor="primary" caps fit>JavaScript Display Possibilities</Heading>
+          <Heading size={3} textColor="primary" caps fit>Expressions/Statements</Heading>
+          <Text margin="10px 0 0" textColor="secondary" size={1} bold>
+            An expression is any valid unit of code that resolves to a value.
+          </Text>
+          <Text margin="10px 0 0" textColor="secondary" size={1} bold>
+            Statements are "instructions" to be "executed"
+          </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={3} textColor="secondary" caps fit>Conditional Statements:</Heading>
           <List>
-            <ListItem>innerHTML</ListItem>
-            <ListItem>document.write();</ListItem>
-            <ListItem>window.alert()</ListItem>
-            <ListItem>console.log();</ListItem>
+            <ListItem>The if Statement</ListItem>
+            <ListItem>The else Statement </ListItem>
+            <ListItem>The else if Statement </ListItem>
+            <ListItem>Switch Case</ListItem>
           </List>
         </Slide>
 
-        <Slide transition={["zoom"]} bgColor="tertiary">
-          <Heading size={3} textColor="primary" caps fit>Javascript Variables</Heading>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={3} textColor="secondary" caps fit>Containers for storing data values:</Heading>
-          <List>
-            <ListItem>var name = "dhawal";</ListItem>
-            <ListItem>var x = 6;</ListItem>
-            <ListItem>var x = 10, y = 6;</ListItem>
-            <ListItem>var z = x + y;</ListItem>
-            <ListItem>var person = "John Doe", carName = "Volvo", price = 200;</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={3} textColor="secondary" caps fit>JavaScript Identifiers:</Heading>
-          <List>
-            <ListItem>Names can contain letters, digits, underscores, and dollar signs.</ListItem>
-            <ListItem>Names must begin with a letter</ListItem>
-            <ListItem>Names are case sensitive (y and Y are different variables)</ListItem>
-            <ListItem>Reserved words (like JavaScript keywords) cannot be used as names eg: class, new, var etc</ListItem>
-            <ListItem>Use camel case</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={3} textColor="secondary" caps fit>Assignment Operator</Heading>
-          <List>
-            <ListItem> = </ListItem>
-            <ListItem>RHS is assigned to LHS</ListItem>
-            <ListItem>var x = 6;</ListItem>
-            <ListItem>var z = x + y;</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={3} textColor="secondary" caps fit>JavaScript Data Types & Declaration</Heading>
-          <List>
-            <ListItem>string and number for now, many other</ListItem>
-            <ListItem>var x = 30; //number </ListItem>
-            <ListItem>var x = "dhawal mehta"; //string </ListItem>
-            <ListItem>var x = "30"; //string</ListItem>
-            <ListItem>var carName = "Volvo";var carName; doesnt lose value</ListItem>
-          </List>
-        </Slide>
+
+
+
+
 
         <Slide transition={["zoom"]} bgColor="tertiary">
           <Heading size={3} textColor="primary" caps fit>Q&A</Heading>
@@ -154,12 +141,9 @@ export default class Presentation extends React.Component {
             Check Email
           </Text>
           <Text margin="10px 0 0" textColor="secondary" size={3} fit bold>
-            Last Date & Time of Submission: 5th April, Thursday by 3pm.
+            Last Date & Time of Submission: 9th April, Monday by 9pm.
           </Text>
         </Slide>
-
-
-
 
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <Heading size={3} textColor="primary" caps fit>Thank you!</Heading>
